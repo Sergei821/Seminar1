6429c8b103f15d2599d2e65123169b336c5b3bf1
Sergei821 <ps16111982@gmail.com>
2023-04-08 19:36:59 +0300

    2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, 
+//является ли второе число квадратом первого.
+//a = 5; b = 25 -> да 
+//a = 2 b = 10 -> нет 
+//a = 9; b = -3 -> нет 
+//a = -3 b = 9 -> да
+
+
+Console.Write("Укажите число > ");  // Вывод на экран фразы 
+string value;                       // Объявление переменной типа строка
+value = Console.ReadLine();         // Считываем с консоли строку
+int numberA = Convert.ToInt32(value);
+
+Console.Write("Укажите второе число > ");  // Вывод на экран фразы                       // Объявление переменной типа строка
+value = Console.ReadLine();         // Считываем с консоли строку
+int numberB = Convert.ToInt32(value);
+if (numberB == numberA * numberA)
+{
+    System.Console.WriteLine("Да является");
+}
+else
+{
+    System.Console.WriteLine("Нет не является ");
+}
+
